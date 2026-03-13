66d08031a937d9cd94909a16da48f85b630b5153
Lovethrech <rachealdolapo45@gmail.com>
2026-03-13 23:25:16 +0100

    ...working on contact section.

---
+++ action.js
@@ -0,0 +1,6 @@
+var fullName=document.getElementById("fullname");
+var email=document.getElementById("email");
+var phoneNumber=document.getElementById("phoneNumber");
+var message=document.getElementById("message");
+
+console.log(`The details submitted are as follows: ${fullName}, ${email}, ${phoneNumber} with the message [${message}]`);
