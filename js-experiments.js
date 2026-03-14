7d8fae6e2c1cb22d0ac6d3e3709929a5f45ae400
Lovethrech <rachealdolapo45@gmail.com>
2026-03-14 23:00:16 +0100

    Learning scope, hoisting and closures.

---
+++ js-experiments.js
@@ -0,0 +1,9 @@
+let globalVar="I'm everywhere";
+
+const testScope=()=>{
+    let localVar="I'm only in this function";
+    if(true){
+        let blockVar="I'm only in this block";
+        
+    }
+}
