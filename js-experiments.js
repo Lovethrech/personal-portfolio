98651f2fd7b41152edffdfdfeb6315b2c3a48a8a
Lovethrech <rachealdolapo45@gmail.com>
2026-03-14 23:13:23 +0100

    Learning scope, hoisting and closures.

--- js-experiments.js
+++ js-experiments.js
@@ -41,3 +41,8 @@ function createCounter(){
     };
 }
 const counter1=createCounter();
+console.log(counter1());
+console.log(counter1());
+
+const counter2=createCounter();
+console.log(counter2());
