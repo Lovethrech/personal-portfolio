19978eceb7e1e99def1f09e8519d245d8a7db280
Lovethrech <rachealdolapo45@gmail.com>
2026-03-14 17:43:18 +0100

    ...working on dark theme features.

--- action.js
+++ action.js
@@ -1,9 +1,1 @@
-var fullName=document.getElementById("fullname");
-var email=document.getElementById("email");
-var phoneNumber=document.getElementById("phoneNumber");
-var message=document.getElementById("message");
-
-const btnSubmit=()=>{
-    console.log(`The details submitted are as follows: ${fullName.value}, ${email.value}, ${phoneNumber.value} with the message [${message.value}]`);
-}
-window.setInterval(btnSubmit,50000);
+document.addEventListener('DOMContentLoaded',)
